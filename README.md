fd85537395f98c9763ade55490cbf8715d652473
kmgulledge <kmgulledge@gmail.com>
2017-06-24 23:50:52 -0400

    lets hope this works

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # week-4-game
+
+Star Wars Themed Crystal Collection Game
+
+Here's how the app works:
+There will be four droids displayed as buttons on the page.
+The player will be shown a random number at the start of the game.
+When the player clicks on a droid, it will add a specific amount of points to the player's total score.
+When the player does click one of the droids, the players score counter will update.
+The player wins if their total score matches the random number from the beginning of the game.
+The player loses if their score goes above the random number.
+The game restarts whenever the player wins or loses.
+When the game begins again, the player will see a new random number. Also, all the droids will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+The app shows the number of games the player wins and loses. 
+The random number shown at the start of the game is between 19 - 120.
+Each droid has a random hidden value between 1 - 12.
---
+++ assets/javascript/game.js
@@ -0,0 +1,102 @@
+// Begin White Belt Javascript / Jquery Code
+
+// Selects a random number between 19 - 120 to be shown at the start of the game
+$( document ).ready(function(){
+  var Random=Math.floor(Math.random() * 120)
+
+// Puts the random number to the HTML 
+$("#randomNumber").text(Random);
+  
+// Setting up random numbers for each jewel between 1 - 12
+var num1= Math.floor(Math.random() * 12)
+var num2= Math.floor(Math.random() * 12)
+var num3= Math.floor(Math.random() * 12)
+var num4= Math.floor(Math.random() * 12)
+  
+//  Variables
+var userTotal= 0; 
+var wins= 0;
+var losses = 0;
+  
+$("#numberWins").text(wins);
+$("#numberLosses").text(losses);
+
+// Resets the game
+function reset(){
+  Random=Math.floor(Math.random() * 120);
+  console.log(Random);
+$("#randomNumber").text(Random);
+  num1= Math.floor(Math.random() * 12);
+  num2= Math.floor(Math.random() * 12);
+  num3= Math.floor(Math.random() * 12);
+  num4= Math.floor(Math.random() * 12);
+  userTotal= 0;
+$("#finalTotal").text(userTotal);
+} 
+
+// Adds the win to the Total
+function win(){
+  alert("You won all the droids!");
+  wins++; 
+  $("#numberWins").text(wins);
+  reset();
+}
+
+// Adds the loss to the Total
+function loser(){
+  alert ("Oh no, you lost all your droids!");
+  losses++;
+  $("#numberLosses").text(losses);
+  reset()
+}
+
+// Sets up click for jewels
+$("#one").on ("click", function(){
+  userTotal = userTotal + num1;
+  console.log("New userTotal= " + userTotal);
+$("#finalTotal").text(userTotal); 
+         
+  // Sets win/lose 
+  if (userTotal == Random){
+      win();
+} else if ( userTotal > Random){
+      loser();
+  }   
+}) 
+
+$("#two").on ("click", function(){
+  userTotal = userTotal + num2;
+  console.log("New userTotal= " + userTotal);
+$("#finalTotal").text(userTotal); 
+   if (userTotal == Random){
+        win();
+} else if ( userTotal > Random){
+          loser();
+  } 
+})  
+  
+$("#three").on ("click", function(){
+  userTotal = userTotal + num3;
+  console.log("New userTotal= " + userTotal);
+$("#finalTotal").text(userTotal);
+
+// Sets win/lose 
+  if (userTotal == Random){
+        win();
+} else if ( userTotal > Random){
+          loser();
+  } 
+})  
+  
+$("#four").on ("click", function(){
+  userTotal = userTotal + num4;
+  console.log("New userTotal= " + userTotal);
+$("#finalTotal").text(userTotal); 
+      
+  if (userTotal == Random){
+      win();
+} else if ( userTotal > Random){
+          loser();
+    }
+  });   
+}); 
